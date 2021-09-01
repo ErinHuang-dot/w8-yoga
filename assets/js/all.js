cbfa97cfe0fca06bd6237f1069223114c76ac1e3
Erin Huang <erin.huang09@gmail.com>
2021-09-01 21:40:13 +0800

    Update 2021-09-01T13:40:09.255Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,103 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$('.icon-ham').click(function (e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active');
 });
+var imgOverlaySwiper = document.querySelector('.imgOverlaySwiper');
+
+if (imgOverlaySwiper) {
+  var swiper = new Swiper('.imgOverlaySwiper', {
+    slidesPerView: 2.5,
+    spaceBetween: 30
+  });
+}
+
+var reviewSwiper = document.querySelector('.reviewSwiper');
+
+if (reviewSwiper) {
+  var _swiper = new Swiper('.reviewSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grid: {
+      rows: 3,
+      fill: 'row'
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      }
+    } // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+
+  });
+}
+"use strict";
+
+var imgOverlaySwiper = document.querySelector('.imgOverlaySwiper');
+
+if (imgOverlaySwiper) {
+  var swiper = new Swiper('.imgOverlaySwiper', {
+    slidesPerView: 2.5,
+    spaceBetween: 30
+  });
+}
+
+var reviewSwiper = document.querySelector('.reviewSwiper');
+
+if (reviewSwiper) {
+  var _swiper = new Swiper('.reviewSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grid: {
+      rows: 3,
+      fill: 'column'
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 2,
+          fill: 'row'
+        }
+      }
+    } // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+
+  });
+}
 //# sourceMappingURL=all.js.map
