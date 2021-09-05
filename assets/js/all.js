cad53acb043b9f78a93ea3227b9ddba918498339
Erin Huang <erin.huang09@gmail.com>
2021-09-05 14:49:07 +0800

    Update 2021-09-05T06:49:02.435Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,53 +4,20 @@ $('.icon-ham').click(function (e) {
   e.preventDefault();
   $('.dropdown').toggleClass('active');
 });
-var imgOverlaySwiper = document.querySelector('.imgOverlaySwiper');
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  buttonClass: 'btn secondary',
+  prevArrow: '<',
+  nextArrow: '>'
+});
+"use strict";
 
-if (imgOverlaySwiper) {
-  var swiper = new Swiper('.imgOverlaySwiper', {
-    slidesPerView: 2.5,
-    spaceBetween: 30
-  });
-}
-
-var reviewSwiper = document.querySelector('.reviewSwiper');
-
-if (reviewSwiper) {
-  var _swiper = new Swiper('.reviewSwiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    grid: {
-      rows: 3,
-      fill: 'row'
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        grid: {
-          rows: 2,
-          fill: 'row'
-        }
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        grid: {
-          rows: 2,
-          fill: 'row'
-        }
-      }
-    } // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
-
-  });
-}
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  buttonClass: 'btn secondary',
+  prevArrow: '<',
+  nextArrow: '>'
+});
 "use strict";
 
 var imgOverlaySwiper = document.querySelector('.imgOverlaySwiper');
@@ -93,11 +60,29 @@ if (reviewSwiper) {
           fill: 'row'
         }
       }
-    } // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
-
+    }
   });
 }
+
+var profileSwiper = document.querySelector('.profileSwiper');
+
+if (profileSwiper) {
+  var _swiper2 = new Swiper('.profileSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  });
+} // pagination: {
+//   el: ".swiper-pagination",
+//   clickable: true,
+// },
 //# sourceMappingURL=all.js.map
